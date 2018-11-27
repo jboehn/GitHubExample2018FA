7734d9f2ec34dfa2123e4294512d48671b058a88
jboehn <jboehning@live.com>
2018-11-27 13:34:57 -0600

    just adding my two cents.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Jody Boehning
+// COSC 1320 001
+// 11/27/2018
+// Sample New Project for GitHub
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Make sure to click to add to Source Control when creating new project for Git integration
+            // Use .NET Framwork 3.5 or higher
+            // Have to actually connect project to GitHub account
+            // click on arrow at bottom of screen
+            // click publish git repo under push to remote repository
+            // Use created project url to publish
+            // create a repository on github.com
+            // make sure it is public
+            // copy url under quick setup
+            // paste url in visual studio
+            // log in
+            // make sure arrow shows 0
+            //  hello
+            // This is my amazing codebase.
+            // When a change is made, check the pencil on bottom of screen, it logs all changes made
+            // click on pencil
+            // have to describe what changes were made.
         }
     }
 }
